9b6e28436f1b96f02736c0a929833706c3bd0356
Bence A. Tóth <tothab@gmail.com>
2020-12-29 19:39:51 +0100

    Removed readme text from solution.js

--- solution.js
+++ solution.js
@@ -1,18 +1,3 @@
-// There is a simple they play with dice.
-
-// There are two players. The game consists of turns.
-// In each turn, the player rolls two dice. If the two
-// numbers are different, then the turn is over and the
-// next player continues. If the two numbers are the same,
-// the player can roll again with one die before the turn ends.
-
-// At the end of the game, each player's score is the sum
-// of all their dice rolls.
-
-// You have to write a JavaScript function that takes
-// an array of individual dice rolls, and returns an array
-// of two numbers with the score of the two players.
-
 // Let's calculate the sum of all the turns in the playerTurns array
 const calculateOverallScoreOfPlayer = (playerTurns) => {
   return playerTurns.flat().reduce((a, b) => a + b, 0);
